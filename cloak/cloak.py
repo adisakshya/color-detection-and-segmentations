e5934bb89f2b3da1ed6a8385a15058b4487cfb4d
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-08-31 22:32:37 +0530

    capture background

--- cloak/cloak.py
+++ cloak/cloak.py
@@ -1,3 +1,19 @@
-import cv2
+import cv2, time, numpy as np
 
 print('OpenCV Version:',cv2.__version__)
+
+# video capture
+capture_video = cv2.VideoCapture(0)
+
+
+time.sleep(1)
+count = 0
+background = 0
+
+# capture background
+for i in range(60):
+    flag, background = capture_video.read()
+    if not flag:
+        continue
+
+background = np.flip(background, axis=1)
