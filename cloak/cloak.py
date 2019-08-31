df3e227bce116ac87a0399490aef021a2c08b04b
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-09-01 00:38:21 +0530

    convert image-bgr2hsv

--- cloak/cloak.py
+++ cloak/cloak.py
@@ -17,3 +17,17 @@ for i in range(60):
         continue
 
 background = np.flip(background, axis=1)
+
+# read from video
+while (capture_video.isOpened()):
+
+        flag, img = capture_video.read()
+        if not flag:
+        	break
+		count += 1
+
+		img = np.flip(img, axis=1)
+
+		# convert image - BGR to HSV
+		# focus on detection of red color
+		hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
