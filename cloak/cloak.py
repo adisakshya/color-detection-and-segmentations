b5b901a89c8dca5590f6fffd330b52005e24563c
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-09-01 20:43:27 +0530

    created masks for color detection

--- cloak/cloak.py
+++ cloak/cloak.py
@@ -31,3 +31,19 @@ while (capture_video.isOpened()):
 	# convert image - BGR to HSV
 	# focus on detection of red color
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # generate mask to detect red color
+    l_red = np.array([100, 40, 40])
+    u_red = np.array([100, 255, 255])
+    mask_a = cv2.inRange(hsv, l_red, u_red)
+
+    l_red = np.array([155, 40, 40])
+    u_red = np.array([180, 255, 255])
+    mask_b = cv2.inRange(hsv,l_red, u_red)
+
+    mask = mask_a + mask_b
+
+    # Refining the mask corresponding to the detected red color
+    mask_a = cv2.morphologyEx(mask_a, cv2.MORPH_OPEN, np.ones((3,3),np.uint8),iterations=2)
+    mask_b = cv2.dilate(mask_a,np.ones((3,3),np.uint8),iterations = 1)
+    mask_b = cv2.bitwise_not(mask_a)
