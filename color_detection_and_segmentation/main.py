4fbd8f39831e4be4c8e7e482b839998843098887
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2020-03-01 00:56:35 +0530

    removed unwanted comments

--- color_detection_and_segmentation/main.py
+++ color_detection_and_segmentation/main.py
@@ -1,19 +1,9 @@
 import cv2
 import numpy as np
 import time
-#harry potter uses that cloak to become invisible 
-#color dectection technique and segmentation 
-#in greeen screening we remove background in this technique
 
-# alias python='/Library/Frameworks/Python.framework/Versions/3.6/bin/python3.6'
-# replace the pixels corresponding with background pixels to generate the invisibilty feature
+print('OpenCV Version:', cv2.__version__)
 
-#Hue : This channel encodes color information. Hue can be thought of an angle where 0 degree corresponds to the red color, 
-#      120 degrees corresponds to the green color, and 240 degrees corresponds to the blue color.
-#Saturation : This channel encodes the intensity/purity of color. For example, pink is less saturated than red.
-#$Value : This channel encodes the brightness of color. Shading and gloss components of an image appear in this channel
-#reading the videocapture video  
-print(cv2.__version__)
 capture_video = cv2.VideoCapture(0)
 
 #give the camera to warm up
